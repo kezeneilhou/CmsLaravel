3cb9ea752cc33d336574cfe9363f512a714db93a
Kezeneilhou <mkezneneilhou@gmail.com>
2024-10-28 17:03:12 +0530

    asset publishing added

--- src/Providers/CmsServiceProvider.php
+++ src/Providers/CmsServiceProvider.php
@@ -10,6 +10,14 @@ class CmsServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'cms');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->app->make('Kezeneilhou\\CmsLaravel\\Controllers\\PostController');
+        $this->app->make('Kezeneilhou\\CmsLaravel\\Controllers\\QuillUploadController');
+        $this->publishes(
+            [
+            __DIR__ . '/../resources/views' => resource_path('views'),
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
+            __DIR__.'/../controllers' => app_path('Http/Controllers'),
+            ],'laravel-cms');
     }
 
     public function register()
