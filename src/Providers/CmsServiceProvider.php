1edc3128ed6f9830f2db83980b12a15159a7eadf
Kezeneilhou <mkezneneilhou@gmail.com>
2024-10-28 16:44:03 +0530

    path fixed

--- src/Providers/CmsServiceProvider.php
+++ src/Providers/CmsServiceProvider.php
@@ -7,9 +7,9 @@ class CmsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/resources/views', 'cms');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'cms');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 
     public function register()
